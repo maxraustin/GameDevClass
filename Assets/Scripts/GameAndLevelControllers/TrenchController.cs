53422df9e82fb30cce7c73b726f70bb843b025b9
cwardell77 <corey.wardell123@gmail.com>
2016-11-07 20:51:14 -0700

    Trench level (#23)
    * Found little bug
    
    Fixed it so player ship spawned
    
    * level 2
    
    level 2
    
    * resolving conflict
    
    * resolving conflicts
    
    * Merge conflicts
    
    * confict fix
    
    * conflict fix
    
    just getting conflicts resolved
    
    * conflict change
    
    * fix conflict
    
    * TrenchLevelUpdated
    
    Certain points will spawn units
    
    * Level 3 updates
    
    playble, still needs turrets
    
    * Added turrets to trench
    
    Added to many, but working on optimization.

---
+++ Assets/Scripts/GameAndLevelControllers/TrenchController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrenchController : BaseLevelController {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    protected override void PlayerEnteredBoundary(GameObject boundary)
+    {
+        if (boundary == boundaries[0])
+        {
+            HUDController.Instance.DisplayMessage("Fly Low!", 2.0f);
+        }else if (boundary == boundaries[1])
+        {
+            GameController.Instance.PlayerVictorious();
+        }else if (boundary == boundaries[2])
+        {
+            UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 3, enemySpawns[0]);
+        }
+        else if (boundary == boundaries[3])
+        {
+            UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 3, enemySpawns[1]);
+        }
+        else if (boundary == boundaries[4])
+        {
+            UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 10, enemySpawns[2]);
+        }
+        else if (boundary == boundaries[5])
+        {
+            UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 4, enemySpawns[3]);
+        }
+        else if (boundary == boundaries[6])
+        {
+            UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 10, enemySpawns[4]);
+        }
+        else if (boundary == boundaries[7])
+        {
+            UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 8, enemySpawns[5]);
+        }
+        else if (boundary == boundaries[8])
+        {
+            UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 15, enemySpawns[6]);
+        }
+    }
+
+
+    protected override void PlayerLeftBoundary(GameObject boundary)
+    {
+        if (boundary == boundaries[0])
+        {
+            HUDController.Instance.DisplayMessage("Safer altitude", 2.0f);
+        }
+    }
+}
